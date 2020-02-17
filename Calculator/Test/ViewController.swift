88a827e474e9c8f1b32f42232862e2ef3a2a8ff7
ZhenyaGrinev <grinev.zhenya1986@gmail.com>
2020-02-17 15:39:10 -0500

    edit 17.02.2020

--- Calculator/Test/ViewController.swift
+++ Calculator/Test/ViewController.swift
@@ -13,105 +13,70 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-    
+    var errorClear = false
     @IBOutlet weak var display: UILabel!
-    var first :Bool=false
+    @IBOutlet weak var history: UILabel!
     @IBAction func addnumber(sender: UIButton) {
-        if(!first){
-            display.text!=""
-            first=true
-        }
-        if(sender.currentTitle==nil || display.text==nil){
-            return
-        }
-        let digit = sender.currentTitle
-            if(display.text!=="" && digit == "."){
-                return
+        errorClearFunc()
+        if let text = display.text, let digit = sender.currentTitle {
+            if(text.count>=13){return}
+            if (digit.elementsEqual(".")) {
+                if (text.last=="." || text.isEmpty || text.contains(".")){
+                    return
+                } else {
+                    display.text! += digit
+                }
+            } else{
+                display.text! += digit
             }
-            display.text! += digit!
+        }
+        
     }
     var firstval : Float = 0
     var secondval : Float = 0
     var resultat : Float = 0
     var act : String = "0"
     
-    @IBAction func plusact(sender: UIButton) {
-        if(display.text! == "" || display.text==nil){
-            return
+    @IBAction func operation(sender: UIButton) {
+        errorClearFunc()
+        if let text = display.text,let digit = sender.currentTitle {
+            if text.isEmpty && digit.isEmpty {return}
+            if let  num  = Float(text){
+                firstval = num
+                act = digit
+                display.text =  ""
+            }
         }
-        firstval = Float(display.text!)!
-        act = "+"
-        display.text =  ""
-    }
-    
-    @IBAction func minusact(sender: UIButton) {
-        if(!first){
-            display.text!=""
-            first=true
-        }
-        /*if display.text?.isEmpty ?? true {
-            display.text!
-        }
-        
-        if let text = display.text, !text.isEmpty {
-            text
-        }
-        guard let text = display.text else {print("Error getting value") ; return}
-        text
-        
-        if display.text != nil, display.text!.isEmpty {
-            display.text!
-        }*/
-        if(display.text! == "" || display.text==nil){
-            let digit  = sender.currentTitle
-            display.text! += digit!
-            return
-        }
-        firstval = Float(display.text!)!
-        act = "-"
-        display.text =  ""
-    }
-    
-    @IBAction func umnact(sender: UIButton) {
-        if(display.text! == "" || display.text==nil){
-            return
-        }
-        firstval = Float(display.text!)!
-        act = "*"
-        display.text =  ""
-    }
-    
-    @IBAction func delit(sender: UIButton) {
-        if(display.text! == "" || display.text==nil){
-            return
-        }
-        firstval = Float(display.text!)!
-        act = "/"
-        display.text =  ""
-    }
-    @IBAction func percentOfDiv(sender: UIButton) {
-        if(display.text! == "" || display.text==nil){
-            return
-        }
-        firstval = Float(display.text!)!
-        act = "%"
-        display.text =  ""
     }
     @IBAction func clearact(sender: UIButton) {
+        errorClearFunc()
         firstval = 0
         secondval = 0
         act = ""
-        if(display.text==nil){
-            return
+        if display.text != nil {
+            display.text =  ""
         }
-        display.text =  "0"
+    }
+    @IBAction func change(sender: UIButton) {
+        errorClearFunc()
+        if var text = display.text {
+            if (text.isEmpty || text.last=="."){return}
+            if(text.first=="-"){
+                text.removeFirst();
+            }
+            else{
+                text="-"+text
+            }
+            display.text = text
+        }
     }
     
     @IBAction func ravnoact(sender: UIButton) {
-        if(display.text! == "" || display.text==nil){
-            return
-        }
-        secondval = Float(display.text!)!
+        errorClearFunc()
+        if let text = display.text {
+        if text.isEmpty{return}
+            if let num = Float(text){
+        secondval=num
         switch act {
         case "+":
             resultat = firstval + secondval
@@ -120,15 +85,51 @@ class ViewController: UIViewController {
         case "*":
             resultat = firstval * secondval
         case "/":
+            if(secondval==0){display.text="Not divide by zero";errorClear = true;return}
             resultat = firstval / secondval
         case "%":
             resultat = firstval.truncatingRemainder(dividingBy: secondval)
         default:
             return
         }
+        if history.text != nil {
+        history.text = "\(firstval)\(act)\(secondval)"
+        }
         display.text = String(resultat)
         act=""
-        
+            }
+        }
+    }
+    func errorClearFunc(){
+        if(errorClear){
+            errorClear = false
+            if(display.text != nil){
+                display.text=""
+            }
+        }
+    }
+    @IBAction func back(sender: UIButton) {
+        if(!act.isEmpty){act=""}
+        else{
+            if let text = display.text{
+                if(text.isEmpty){return}
+                else{
+                    display.text?.removeLast()
+                }
+            }
+        }
     }
 }
+/*if display.text?.isEmpty ?? true {
+    display.text!
+}
 
+if let text = display.text, !text.isEmpty {
+    text
+}
+guard let text = display.text else {print("Error getting value") ; return}
+text
+
+if display.text != nil, display.text!.isEmpty {
+    display.text!
+}*/
